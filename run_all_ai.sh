39b56e695682158d5f192bab5c3316cc58c0b33e
Josh Long <54473+joshlong@users.noreply.github.com>
2025-04-09 00:38:56 -0700

    adding minimax test
    Signed-off-by: Josh Long <54473+joshlong@users.noreply.github.com>

--- run_all_ai.sh
+++ run_all_ai.sh
@@ -4,8 +4,8 @@
 #./gradlew :ai:spring-ai-starter-model-openai:build
 #./gradlew :ai:spring-ai-starter-model-openai:appTest
 
-old="spring-ai-starter-model-openai spring-ai-starter-model-ollama"
-for project in spring-ai-starter-model-anthropic ; do
+old="spring-ai-starter-model-openai spring-ai-starter-model-ollama  spring-ai-starter-model-anthropic"
+for project in spring-ai-starter-model-minimax ; do
   echo "testing... $project "
   ./gradlew   -PfromMavenLocal=org.springframework.ai :ai:${project}:nativeAppTest
 #  ./gradlew --scan -PfromMavenLocal=org.springframework.ai :ai:${project}:nativeAppTest
